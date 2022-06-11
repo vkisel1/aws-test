572fdf6316460ca8b9d3d9bc1bdd101c7c4bdb83
vkisel1 <107317459+vkisel1@users.noreply.github.com>
2022-06-11 21:41:19 +0300

    Update Startup.cs

--- AwsTestProject/Startup.cs
+++ AwsTestProject/Startup.cs
@@ -40,7 +40,7 @@ namespace AwsTestProject
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
